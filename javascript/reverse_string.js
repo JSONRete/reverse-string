85906447ce4ff6d0be5bc96f2834b3f4341119e6
JSONRete <jsonrete@gmail.com>
2023-03-04 00:00:28 -0500

    first add .

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,19 @@
 function reverseString(str) {
   // type your code here
+    if (!str.length) {
+    return str
+}
+
 }
 
 if (require.main === module) {
   // add your own tests in here
+    let startingEnd = ''
+
+  for (let index = str.length - 1; index >= 0; index--) {
+    startingEnd = startingEnd + str[index];
+  }
+  return startingEnd
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
